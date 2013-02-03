a2183944ba6f9d79f592e0312508859ac38c19d1
Alex Carol <alex.carol.c@gmail.com>
2013-02-03 20:00:40 +0100

    Created entities

---
+++ src/TentacleSoft/Bundle/SiegeCraftBundle/Entity/Unit.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace TentacleSoft\Bundle\SiegeCraftBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Unit
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Unit
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="configId", type="integer")
+     */
+    private $configId;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="idle", type="boolean")
+     */
+    private $idle;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Node, inversedBy="units")
+     */
+    private $node;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Player", mappedBy="units")
+     */
+    private $player;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set configId
+     *
+     * @param integer $configId
+     * @return Unit
+     */
+    public function setConfigId($configId)
+    {
+        $this->configId = $configId;
+    
+        return $this;
+    }
+
+    /**
+     * Get configId
+     *
+     * @return integer 
+     */
+    public function getConfigId()
+    {
+        return $this->configId;
+    }
+
+    /**
+     * Set idle
+     *
+     * @param boolean $idle
+     * @return Unit
+     */
+    public function setIdle($idle)
+    {
+        $this->idle = $idle;
+    
+        return $this;
+    }
+
+    /**
+     * Get idle
+     *
+     * @return boolean 
+     */
+    public function getIdle()
+    {
+        return $this->idle;
+    }
+}
