a6dc7f4e42c51c1715389dca55b8c22215f78524
Roger Clotet <roger.clotet@socialpoint.es>
2013-03-13 15:37:45 +0100

    Fixed typo

--- src/TS/Bundle/SiegeCraftBundle/Controller/DefaultController.php
+++ src/TS/Bundle/SiegeCraftBundle/Controller/DefaultController.php
@@ -59,7 +59,7 @@ class DefaultController extends Controller
         $units = array();
         /*
          * Each of the nodes will have different information depending on the type
-         * so there should be a kind of input saying the position of the node asqued for
+         * so there should be a kind of input saying the position of the node asked for
          * and also a service for looking up what kind of node it is.
          */
 
