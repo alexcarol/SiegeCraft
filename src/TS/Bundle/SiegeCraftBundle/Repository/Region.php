f218ed0802c17e6a108bb817c9eb2fe5dd20fd2b
Alex Carol <alex.carol.c@gmail.com>
2013-03-16 22:36:48 +0100

    Fixed assignRegion

--- src/TS/Bundle/SiegeCraftBundle/Repository/Region.php
+++ src/TS/Bundle/SiegeCraftBundle/Repository/Region.php
@@ -10,15 +10,20 @@ class Region extends EntityRepository {
      * @param $fortressId
      */
     public function assignRegion($fortressId) {
-        $sql = 'UPDATE Region ' .
-               'SET fortress_id = :fortress ' .
-               'WHERE id = (' .
+        //$em = $this->getEntityManager();
+        //$this->createQueryBuilder()->select()
+
+        $sql = 'UPDATE Region r1 ' .
+               'SET r1.fortress_id = :fortress ' .
+               'WHERE r1.id = (' .
                     'SELECT reg.id ' .
-                    'FROM (SELECT r.id ' .
+                    'FROM (SELECT r.id, r.zone_id ' .
                           'FROM Region r ' .
                           'WHERE r.fortress_id IS NULL ' .
                           'ORDER BY RAND() ' .
-                          'LIMIT 1) reg )';
+                          'LIMIT 1) reg '.
+                    'WHERE reg.id = r1.id AND reg.zone_id = r1.zone_id ' .
+                ')';
         $params = array(
             'fortress' => $fortressId
         );
