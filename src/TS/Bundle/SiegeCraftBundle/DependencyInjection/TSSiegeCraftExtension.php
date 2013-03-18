104fe67a7cedc2f1ee28b4fb8ebc0c49320c4a56
Alex Carol <alex.carol.c@gmail.com>
2013-03-18 22:54:06 +0100

    Added first tabs to config and validation through container

--- src/TS/Bundle/SiegeCraftBundle/DependencyInjection/TSSiegeCraftExtension.php
+++ src/TS/Bundle/SiegeCraftBundle/DependencyInjection/TSSiegeCraftExtension.php
@@ -21,6 +21,9 @@ class TSSiegeCraftExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        $container->setParameter('ts_siege_craft.tabs', $config['tabs']);
+
+
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
