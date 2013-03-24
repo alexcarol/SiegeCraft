cb96f2dda18c378e5d59362311a7f0b68235d795
Alex Carol <alex.carol.c@gmail.com>
2013-03-24 19:42:13 +0100

    Removed exception from listener

--- src/TS/Bundle/SiegeCraftBundle/Security/Firewall/ExceptionListener.php
+++ src/TS/Bundle/SiegeCraftBundle/Security/Firewall/ExceptionListener.php
@@ -10,7 +10,6 @@ class ExceptionListener extends BaseExceptionListener
 {
     protected function setTargetPath(Request $request)
     {
-        throw new \Exception('Locale = ' . $request->getLocale() . ' session ' . json_encode($request->getSession()->all()) . ' uri = ' . $request->getUri());
         $request->getSession()->set('_security.target_path', $request->getUri());
     }
 }
